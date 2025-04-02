75d8351d495c7fa0b88b5a4570c926311cb75dd9
an-lee <an.lee.work@gmail.com>
2025-04-02 17:08:47 +0800

    add tanstack router

--- enjoy-next/src/renderer/app.tsx
+++ enjoy-next/src/renderer/app.tsx
@@ -1,4 +1,27 @@
-import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root");
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
--- enjoy-next/vite.renderer.config.ts
+++ enjoy-next/vite.renderer.config.ts
@@ -1,4 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// eslint-disable-next-line import/no-unresolved
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "node:path";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/renderer/routes",
+      generatedRouteTree: "./src/renderer/routeTree.gen.ts",
+    }),
+    react(),
+  ],
+  resolve: {
+    preserveSymlinks: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@renderer": path.resolve(__dirname, "./src/renderer"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "vendor/pdfjs": path.resolve(
+        __dirname,
+        "./node_modules/foliate-js/vendor/pdfjs"
+      ),
+    },
+  },
+});
