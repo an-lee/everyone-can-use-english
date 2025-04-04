49d1b8314d8dd36d8945972ae741612d576fb668
an-lee <an.lee.work@gmail.com>
2025-04-04 15:19:56 +0800

    setup i18n

--- enjoy-next/src/renderer/components/auth/login-form.tsx
+++ enjoy-next/src/renderer/components/auth/login-form.tsx
@@ -10,16 +10,18 @@ import {
   Label,
 } from "@renderer/components/ui";
 import { Icon } from "@iconify/react";
+import { useTranslation } from "react-i18next";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const { t } = useTranslation("components/auth");
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardTitle className="text-xl">{t("welcome")}</CardTitle>
         </CardHeader>
         <CardContent>
           <form>
@@ -27,11 +29,11 @@ export function LoginForm({
               <div className="flex flex-col gap-4">
                 <Button variant="outline" className="w-full">
                   <Icon icon="cib:apple" className="size-4 mr-2" />
-                  Login with Apple
+                  {t("loginWithApple")}
                 </Button>
                 <Button variant="outline" className="w-full">
                   <Icon icon="cib:google" className="size-4 mr-2" />
-                  Login with Google
+                  {t("loginWithGoogle")}
                 </Button>
               </div>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
---
+++ enjoy-next/src/renderer/lib/i18n.ts
@@ -0,0 +1,27 @@
+// In src/renderer/i18n.ts
+import * as i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend"; // Need to add this dependency
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: "/locales/{{ns}}/{{lng}}.json",
+      addPath: "/locales/{{ns}}/{{lng}}.missing.json",
+    },
+    ns: ["common", "components"], // Default namespaces
+    defaultNS: "common",
+    lng: "en",
+    fallbackLng: "en",
+    supportedLngs: ["en", "zh-CN"],
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["localStorage", "navigator"],
+    },
+  });
+
+export default i18n;
